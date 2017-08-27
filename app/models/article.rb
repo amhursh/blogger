3382d31ea54cfd307eefdcef60162ae81088144d
amhursh <amhursh@gmail.com>
2017-08-27 01:49:39 -0600

    Add tag_list and tag_list= methods to article controller

--- app/models/article.rb
+++ app/models/article.rb
@@ -9,4 +9,14 @@ class Article < ApplicationRecord
     end.join(", ")
   end
 
+  def tag_list=(tags_string)
+    tag_names = (tags_string.split(",")
+                .collect{|s| s.strip.downcase}
+                .uniq)
+    new_or_found_tags = tag_names.collect do |name|
+      Tag.find_or_create_by(name: name)
+    end
+    self.tags = new_or_found_tags
+  end
+
 end
