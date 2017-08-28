f9035c9d6f67cc55102bb6ce9315ca05d3923050
amhursh <amhursh@gmail.com>
2017-08-27 22:55:31 -0600

    Tag delete

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,11 +1,25 @@
 class TagsController < ApplicationController
 
+  before_action :set_tag, only: [:destroy]
+
   def show
     @tag = Tag.find(params[:id])
   end
 
   def index
     @tags = Tag.all
+  end
+
+  def destroy
+    @tag.destroy
+
+    redirect_to tags_path
+  end
+
+  private
+
+  def set_tag
+    @tag = Tag.find(params[:id])
   end
 
 end
